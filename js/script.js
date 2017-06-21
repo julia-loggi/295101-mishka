ee74161095667856ab2101190830b3b0ee1d5874
JulieSemenova <julie.logg@gmail.com>
2017-06-21 18:47:35 +0300

    style

--- js/script.js
+++ js/script.js
@@ -1,26 +1,8 @@
-// var link = document.querySelector(".week-offer__btn");
-// var basket = document.querySelector(".catalog__item-icon");
-// var popup = document.querySelector(".modal");
-// var overlay = document.querySelector(".modal__overlay");
-// var close = popup.querySelector(".modal--close");
-
-
-// link.addEventListener("click", function(event)  {
-//   event.preventDefault();
-//   popup.classList.add("modal--show");
-//   overlay.classList.add("modal__overlay--show");
-// });
-
-
-// window.addEventListener("keydown", function(event) {
-//   if (event.keyCode === 27) {
-//     popup.classList.remove("modal--show");
-//     overlay.classList.remove("modal__overlay--show");
-//   }
-// });
-
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var link = document.querySelector(".add-to-basket__call");
+var popup = document.querySelector(".add-to-basket");
+var overlay = document.querySelector(".add-to-basket__overlay");
 
 navMain.classList.remove('main-nav--nojs');
 navMain.classList.remove('main-nav--opened');
@@ -35,3 +17,18 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+link.addEventListener("click", function(event)  {
+  event.preventDefault();
+  popup.classList.add("add-to-basket--show");
+  overlay.classList.add("add-to-basket__overlay--show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    popup.classList.remove("add-to-basket--show");
+    overlay.classList.remove("add-to-basket__overlay--show");
+  }
+});
+
+
