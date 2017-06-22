2b2defd12046cd035d24d5e75e47c4119f45cf09
JulieSemenova <julie.logg@gmail.com>
2017-06-22 18:15:00 +0300

    style

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
-var link = document.querySelector(".add-to-basket__call");
+var link = document.querySelectorAll(".add-to-basket__call");
 var popup = document.querySelector(".add-to-basket");
 var overlay = document.querySelector(".add-to-basket__overlay");
 
@@ -18,11 +18,13 @@ navToggle.addEventListener('click', function() {
   }
 });
 
-link.addEventListener("click", function(event)  {
-  event.preventDefault();
-  popup.classList.add("add-to-basket--show");
-  overlay.classList.add("add-to-basket__overlay--show");
-});
+for (var i=0; i < link.length; i++) {
+  link[i].addEventListener("click", function(event)  {
+    event.preventDefault();
+    popup.classList.add("add-to-basket--show");
+    overlay.classList.add("add-to-basket__overlay--show");
+  });
+}
 
 window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
@@ -30,5 +32,3 @@ window.addEventListener("keydown", function(event) {
     overlay.classList.remove("add-to-basket__overlay--show");
   }
 });
-
-
